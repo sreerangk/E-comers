4f353edf41598df5501f5ef33db3c5e2547e30d7
sreerangk <sreerangk77@gmail.com>
2022-07-07 18:44:36 +0530

    mongo

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars');
 var app = express();
-
+var fileUpload = require('express-fileupload')
+var db=require('./config/connection')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,7 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
+db.connect((err)=>{
+  if(err)
+  console.log("connection Error"+err);
+  else
+  console.log("database connected to port 27017");
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
